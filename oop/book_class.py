de62a88920d05e91da6d5de4e37873bee78c75f5
Jayne-o <66448516+Jayne-o@users.noreply.github.com>
2025-10-12 00:43:59 -0600

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,17 +1,13 @@
-class Book:
-    def __init__(self, title, author, year):
-        self.title = title
-        self.author = author
-        self.year = year
+from book_class import Book
 
-    def __del__(self):
-        print(f"Deleting {self.title}")
+def main():
+    my_book = Book("1984", "George Orwell", 1949)
+    print(str(my_book))   # Informal string
+    print(repr(my_book))  # Official representation
+    del my_book           # Triggers __del__
 
-    def __str__(self):
-        return f"{self.title} by {self.author}, published in {self.year}"
-
-    def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"
+if __name__ == "__main__":
+    main()
 
   ## To test
 
